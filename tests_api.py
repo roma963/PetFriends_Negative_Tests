109138cc99fe31adc5b5705d702411ad3bab6d1f
roma963 <pith0369@gmail.com>
2025-09-21 00:14:56 +0200

    Добавлены негативные тесты POST/PUT/DELETE с расширенными кодами 4хх

--- tests_api.py
+++ tests_api.py
@@ -2,15 +2,28 @@ import unittest
 import requests
 BASE_URL = 'https://petfriends.skillfactory.ru/'
 class TestPetFriendsNegative(unittest.TestCase):
-    def test_get_pets_wihut_key(self):
-        url = BASE_URL + '/api/pets'
-        res = requests.get(url)
-        self.assertIn(res.status_code, (401, 403), msg=res.text)
-    def test_get_pets_with_invalid_key(self):
-        url = BASE_URL + '/api/pets'
+    def test_get_pet_simple_with_invalid_key(self):
+        """POST: Попытка создать питомца с неверным ключом"""
+        url = BASE_URL + '/api/create_pet_simple'
         headers = {'auth_key': 'invalid_key'}
-        res = requests.get(url, headers=headers)
-        self.assertIn(res.status_code, (401, 403), msg=res.text)
+        data = {'name': 'Barsik', 'animal_type': 'cat' '3'}
+        res = requests.post(url, headers=headers, data=data)
+        self.assertIn(res.status_code, (400, 401, 403, 404, 405), msg=res.text)
+    def test_update_pet_with_invalid_key_or_id(self):
+        """PUT: Попытка обновить данные питомца с неверным ключом/ID"""
+        bad_pet_id = 'nonexistent-id-123'
+        url = BASE_URL + f'/api/pets/{bad_pet_id}'
+        headers = {'auth_key': 'invalid_key'}
+        data = {'name': 'Newname', 'animal_type': 'dog' '5'}
+        res = requests.get(url, headers=headers, data=data)
+        self.assertIn(res.status_code, (400, 401, 403,404, 405), msg=res.text)
+    def test_delete_pet_with_invalid_key_or_id(self):
+        """DELETE: Попытка удалить питомца с неверным ключом/ID"""
+        bad_pet_id = 'nonexistent-id-123'
+        url = BASE_URL + f'/api/pets/{bad_pet_id}'
+        headers = {'auth_key': 'invalid_key'}
+        res = requests.delete(url, headers=headers)
+        self.assertIn(res.status_code, (401, 403, 404), msg=res.text)
 
 if __name__ == '__main__':
     unittest.main()
