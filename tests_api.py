247be11475ee95f964e501132be4ebd4ee1c7fe7
roma963 <pith0369@gmail.com>
2025-09-22 08:50:17 +0200

    Добавлен положительный тест для обновления питомца (PUT)

--- tests_api.py
+++ tests_api.py
@@ -3,32 +3,24 @@ import unittest
 import requests
 BASE_URL = 'https://petfriends.skillfactory.ru/'
 class TestPetFriendsNegative(unittest.TestCase):
-    def test_get_pets_simple_with_valid_key(self):
-        """GET: Проверка: что с действительным ключом список питомцев возвращается (200)"""
-        url = BASE_URL + '/api/pets'
+    def test_update_pet_with_valid_data(self):
+        """PUT: Проверка, что данные питомца можно обновить с валидным ключом """
+        url_create = BASE_URL + 'api/create_pet_simple'
         headers = {'auth_key': os.environ['PF_API_KEY']}
-        res = requests.get(url, headers=headers)
+        data = {'name': 'TempPet', 'animal_type': 'dog', 'age': '1'}
+        res = requests.post(url_create, headers=headers, data=data)
         self.assertEqual(res.status_code, 200, msg=res.text)
-    def test_get_pets_valid_key(self):
-        """GET: Проверка: получение списка питомцев с валидным ключом"""
-        url = BASE_URL + '/api/pets'
-        headers = {'auth_key': os.environ['PF_API_KEY']}
-        res = requests.get(url, headers=headers)
-        self.assertEqual(res.status_code, 200, msg=res.text)
-        data = res.json()
-        self.assertIn('pets', data, msg="В ответе нет ключа 'pets'")
-        self.assertIsInstance(data['pets'],list, msg="Поле 'pets' должно быть списком")
-    def test_add_new_pet_with_valid_data(self):
-        """POST: Проверка: создание нового питомца с корректными данными"""
-        url = BASE_URL + '/api/create_pet_simple'
-        headers = {'auth_key': os.environ['PF_API_KEY']}
-        data = {'name': 'bob', 'animal_type': 'German Shepherd', 'age': '2'}
-        res = requests.post(url, headers=headers, data=data)
+        pet_id = res.json()['id']
+        url_update = BASE_URL + f'api/pets/{pet_id}'
+        new_data = {'name': 'UpdatedPet', 'animal_type': 'cat', 'age': '3'}
+        res = requests.put(url_update, headers=headers, data=new_data)
         self.assertEqual(res.status_code, 200, msg=res.text)
         body = res.json()
-        self.assertEqual(body['name'], 'bob')
-        self.assertEqual(body['animal_type'], "German Shepherd")
-        self.assertEqual(body['age'], '2')
+        self.assertEqual(body['name'], 'UpdatedPet')
+        self.assertEqual(body['animal_type'], 'cat')
+        self.assertEqual(body['age'], '3')
+
+
 if __name__ == '__main__':
     unittest.main()
 
